bc80d7f44752888a488e263dd30e4f0e784384c4
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-09-29 16:40:04 -0300

    feat: componentize card items and add array with pizza data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import ItemsCard from "@/components/items-card";
 import Image from "next/image";
 
 export default function Home() {
@@ -18,39 +17,9 @@ export default function Home() {
 
       <div className="flex flex-col">
         <div className="w-[1237px] h-[746px] bg-amber-100 flex justify-center items-center flex-col">
-          <h1 className="text-2xl mt-[50px] mb-[50px]">Mais vendidas</h1>
-
-          <div className="bg-white w-[1237px] h-[360px] flex items-center">
-            <Card className="w-[280px] h-[300px] justify-center text-center relative bg-[#fefbf4] border-2 border-[#FFF5DE]">
-              <Image
-                src="/icon-favorite.svg"
-                width={32}
-                height={32}
-                alt="icon-favorite"
-                className="absolute top-2 right-2"
-              />
-              <CardHeader>
-                <Image
-                  src="/lacuma-pizza.svg"
-                  width={120}
-                  height={120}
-                  alt="la-cuma"
-                  className="mx-auto"
-                />
-                <CardTitle className="text-[20px] font-semibold">
-                  La Cuma
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-[16px]">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <p className="text-[32px] font-semibold mt-4">R$ 29,90</p>
-                <Button className="rounded-[25px] p-[10px] w-[206px] h-[41px] font-semibold mt-6">
-                  COMPRAR AGORA
-                </Button>
-              </CardContent>
-            </Card>
+          <h1 className="text-[40px] font-semibold mb-[50px]">Mais vendidas</h1>
+          <div>
+            <ItemsCard />
           </div>
         </div>
       </div>
---
+++ components/items-card.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+const ItemsCard = () => {
+  const pizzas = [
+    {
+      id: 1,
+      name: "La Cuma",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: "R$ 29,90",
+      image: "/lacuma-pizza.svg",
+    },
+    {
+      id: 2,
+      name: "La Menta",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: "R$ 25,50",
+      image: "/lamenta-pizza.svg",
+    },
+    {
+      id: 3,
+      name: "La Cuma 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: "R$ 26,00",
+      image: "/lacuma2-pizza.svg",
+    },
+    {
+      id: 4,
+      name: "La Toca",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      price: "R$ 23,80",
+      image: "/latoca-pizza.svg",
+    },
+  ];
+  return (
+    <div className="bg-white w-[1237px] h-[360px] flex items-center gap-x-[39px]">
+      {pizzas.map((pizza) => (
+        <Card
+          key={pizza.id}
+          className="w-[280px] h-[300px] justify-center text-center relative bg-[#fefbf4] border-2 border-[#FFF5DE]"
+        >
+          <Image
+            src="/icon-favorite.svg"
+            width={32}
+            height={32}
+            alt="icon-favorite"
+            className="absolute top-2 right-2"
+          />
+          <CardHeader>
+            <Image
+              src={pizza.image}
+              width={120}
+              height={120}
+              alt={pizza.name}
+              className="mx-auto"
+            />
+            <CardTitle className="text-[20px] font-semibold">
+              {pizza.name}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-[16px]">{pizza.description}</p>
+            <p className="text-[32px] font-semibold mt-4">{pizza.price}</p>
+            <Button className="rounded-[25px] p-[10px] w-[206px] h-[41px] font-semibold mt-6">
+              COMPRAR AGORA
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default ItemsCard;
